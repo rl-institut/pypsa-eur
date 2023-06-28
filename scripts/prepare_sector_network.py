e07af60df654779a8228b962556d9edb7858e9b3
daniel.rdt <daniel.ruedt@campus.tu-berlin.de>
2023-06-28 17:53:33 +0200

    fixed if else gas network shipping

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -2777,7 +2777,7 @@ def add_industry(n, costs):
         # get and sum scenario gas shipping demand
         sce_shi_gas = get_transport_demand_by_subsector(df_sce_tran, 'shipping', 'methane', investment_year)
 
-        if get(options["gas_distribution_grid"], investment_year) == True:
+        if options["gas_network"]:
             # use total pypsa shipping demand to distribute CLEVER gas shipping demand
             pes_shi_reg = all_navigation.to_frame()
             sce_shi_gas_reg = distribute_sce_demand_by_pes_layout(sce_shi_gas, pes_shi_reg, pop_layout)
