28cc2010a502e8270fc5b5ffb437ac49f839d0f6
piaulous <paul.dubielzig@rl-institut.de>
2023-07-11 11:17:13 +0200

    adapt path using snakemake

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -3663,7 +3663,7 @@ def build_sce_cap_prod(input_path_cap, output_path, indicator="capacity"):
 
     # countries modelled and chosen years
     countries = snakemake.config["countries"]
-    years = ["2030", "2040", "2050"] # map(str, snakemake.config["scenario"]["planning_horizons"])
+    years = map(str, snakemake.config["scenario"]["planning_horizons"])
     # import capacities in dictionary
     df_caps = {carr: pd.read_csv(input_path_cap + f"/clever_supply_{indicator}_{carr}.csv",
                                     decimal=',', delimiter=';', index_col=0) \
